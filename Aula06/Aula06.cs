e6fb6d9ba29993ffcdac2fc9b1afebd9c6b65af1
Bruno Álex <alex2804y@gmail.com>
2025-05-02 11:17:50 -0300

    Aulas 6: Formatando a saída no console

--- Aula06/Aula06.cs
+++ Aula06/Aula06.cs
@@ -5,7 +5,30 @@ class Aula06{
 
     static void Main(){
 
-        Console.WriteLine("");
+        //int n1,n2,n3;
+
+        //n1=10; n2=20; n3=30;
+
+        //Console.WriteLine(n1 + ", " + n2 + ", " + n3);
+        //Console.WriteLine("n1={0}, n2={1}, n3={2}",n1,n2,n3);
         
+        // Catactes de escape //
+        //  Quebra de lina
+        //Console.WriteLine("n1={0}\nn2={1}\nn3={2}",n1,n2,n3);
+        //  Tabulação
+        //Console.WriteLine("n1=\t{0}\nn2=\t{1}\nn3=\t{2}",n1,n2,n3);
+        
+        double valorCompra=5.50;
+        double valorVenda=0;
+        double lucro=0.1;
+        string produto="Pastel";
+
+        valorVenda=valorCompra+(valorCompra*lucro);
+
+        Console.WriteLine("Produto........:{0, 15}",produto);
+        Console.WriteLine("Val. Compra....:{0, 15:c}",valorCompra);
+        Console.WriteLine("Lucro.....;....:{0, 15:p}",lucro);
+        Console.WriteLine("Val. Venda.....:{0, 15:c}",valorVenda);
+
     }
 }
