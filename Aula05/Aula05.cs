d45432380a791423be2442b3c18a037954ec326f
Bruno Álex <alex2804y@gmail.com>
2025-05-02 10:26:45 -0300

    Aulas 4 e 5 adcionadas

---
+++ Aula05/Aula05.cs
@@ -0,0 +1,16 @@
+// Operadores e Operações 
+
+using System;
+
+class Aula05{
+    static void Main(){
+
+        //int res = 10 +5;
+        //int res = (10 + 5) * 5;
+
+        bool res = 10 > 5;
+
+        Console.WriteLine(res);
+
+    }
+}
