31010610aa97d096e0cfedb1c8f2795a8e614e28
Bruno Álex <alex2804y@gmail.com>
2025-05-02 10:29:11 -0300

    Aulas 4 e 5 adcionadas

---
+++ Aula04/Aula04.cs
@@ -0,0 +1,15 @@
+// Escopo de variáveis
+
+using System;
+
+
+    class Aula04
+    {
+        static int num = 10;
+
+        static void Main(){
+            int num2 = 0;
+            Console.WriteLine(num);
+        }
+}
+    
