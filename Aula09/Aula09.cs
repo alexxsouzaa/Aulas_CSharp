970ad9367a29ceadd1a112f5c70f5fc5ec5ee5d0
Bruno Álex <alex2804y@gmail.com>
2025-05-03 00:47:32 -0300

    Aulas 9: Operações de Bitwise

---
+++ Aula09/Aula09.cs
@@ -0,0 +1,19 @@
+// Operações de Bitwise
+
+using System;
+
+class Aula09{
+
+    static void Main(){
+
+        int num = 6;
+
+        // Esquerda = Dobra
+        //num=num<<2;
+        // Direita = Metade
+        num=num>>2;
+
+        Console.WriteLine(num);
+    }
+
+}
