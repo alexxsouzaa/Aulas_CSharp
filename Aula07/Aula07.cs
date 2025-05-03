702ee1c9a4336dee6b85deb869061e90bab181ee
Bruno Álex <alex2804y@gmail.com>
2025-05-02 23:57:44 -0300

    Aulas 7: Constantes

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,14 @@
+//   Constantes
+using System;
+
+class Aula07{
+    
+    static void Main(){
+
+        const string nome = "Bruno";
+        const double pi=3.1415;
+
+        Console.WriteLine("Nome: {0}\nPi: {1}", nome, pi);
+
+    }
+}
